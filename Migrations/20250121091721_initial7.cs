c915da764e68165d9a69561eb072ab762fa5f38a
Matesero <grishenkov05@mail.ru>
2025-01-22 01:44:33 +0700

    fix migrations

---
+++ Migrations/20250121091721_initial7.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace courses.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Students",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MidtermResult",
+                table: "Students",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FinalResult",
+                table: "Students",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Students",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MidtermResult",
+                table: "Students",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FinalResult",
+                table: "Students",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
---
+++ Migrations/20250122022919_initial11.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace courses.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial11 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "GroupId",
+                table: "Teachers",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teachers_GroupId",
+                table: "Teachers",
+                column: "GroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Teachers_Groups_GroupId",
+                table: "Teachers",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Teachers_Groups_GroupId",
+                table: "Teachers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Teachers_GroupId",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "GroupId",
+                table: "Teachers");
+        }
+    }
+}
